4b20095ed9450b1dd2a5130a155cd8027f728e18
Haris <muhammadharisyousaf@gmail.com>
2024-03-31 15:14:09 +0500

    Fix the bug with the cmdk latest version

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,4 +1,4 @@
-import {db } from '@/lib/db';
+import {db} from '@/lib/db';
 import {auth} from "@clerk/nextjs";
 import {redirect} from "next/navigation";
 import {IconBadge} from "@/components/icon-badge";
@@ -6,13 +6,20 @@ import {LayoutDashboard} from "lucide-react";
 import TitleForm from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form";
 import DescriptionForm from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form";
 import ImageForm from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form";
+import CategoryForm from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form";
 
-const CourseIdPage = async ({ params }: {params: {courseId: string}}) => {
+const CourseIdPage = async ({params}: { params: { courseId: string } }) => {
     const {userId} = auth();
     if (!userId) return redirect('/');
 
     const course = await db.course.findUnique({where: {id: params.courseId}});
     if (!course) return redirect('/');
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc"
+        }
+    });
 
     const requiredFields = [
         course.title,
@@ -41,12 +48,30 @@ const CourseIdPage = async ({ params }: {params: {courseId: string}}) => {
             </div>
             <div className={"grid grid-cols-1 md:grid-cols-2 gap-6 mt-16"}>
                 <div className="flex items-center gap-x-2">
-                    <IconBadge icon={LayoutDashboard} variant={"default"} />
+                    <IconBadge icon={LayoutDashboard} variant={"default"}/>
                     <h2 className={"text-xl"}>Customize your course</h2>
                 </div>
-                <TitleForm initialData={course} courseId={course.id}/>
-                <DescriptionForm initialData={course} courseId={course.id}/>
-                <ImageForm initialData={course} courseId={course.id} />
+                <TitleForm
+                    initialData={course}
+                    courseId={course.id}
+                />
+                <DescriptionForm
+                    initialData={course}
+                    courseId={course.id}
+                />
+                <ImageForm
+                    initialData={course}
+                    courseId={course.id}
+                />
+                <CategoryForm
+                    initialData={course}
+                    courseId={course.id}
+                    options={
+                        categories.map((category) => ({
+                            label: category.name,
+                            value: category.id
+                        }))}
+                />
             </div>
         </div>
     );
